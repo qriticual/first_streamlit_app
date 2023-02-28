1b0289c08e19ffb541ee8fd4be36f98b80a00535
Jakob Alkass <jakob.alkass@ericsson.com>
2023-02-28 10:13:01 +0100

    restructure with else and if statements

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,15 +26,18 @@ streamlit.dataframe(fruits_to_show)
 
 # New Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try: 
+    fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+    if not fruit_choice:
+        streamlit.error("Please selecta fruit to get information.")
+    else:
+        #streamlit.write('The user entered ', fruit_choice)
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+        streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+    streamlit.error(e)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-# normalize json data
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# makenormalized json data to dataframe
-streamlit.dataframe(fruityvice_normalized)
 
 streamlit.stop()
 
